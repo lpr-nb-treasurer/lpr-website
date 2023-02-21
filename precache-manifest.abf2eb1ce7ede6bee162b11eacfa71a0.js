1cde2798d5c5c50ce1aeb448702d41fefa95cc81
nb.treasurer <nb.treasurer@lambdapsirho.org>
2023-02-20 19:41:46 -0800

    Updates

---
+++ precache-manifest.abf2eb1ce7ede6bee162b11eacfa71a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6dcaeca5f903acda7c",
+    "url": "/lpr-website/static/css/main.9f891846.chunk.css"
+  },
+  {
+    "revision": "1a6dcaeca5f903acda7c",
+    "url": "/lpr-website/static/js/main.9e7e2b3e.chunk.js"
+  },
+  {
+    "revision": "837a6c69178c7f29eb73",
+    "url": "/lpr-website/static/js/runtime~main.8d10752b.js"
+  },
+  {
+    "revision": "c14d811209f160f966f7",
+    "url": "/lpr-website/static/js/2.5a9919cf.chunk.js"
+  },
+  {
+    "revision": "e897de3d059808493f9f",
+    "url": "/lpr-website/static/js/3.5aaf3fca.chunk.js"
+  },
+  {
+    "revision": "33e2380eebaa5a0719916414b2a57438",
+    "url": "/lpr-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.254193202e25ec48237ef817d497f295.js"
+  "/lpr-website/precache-manifest.abf2eb1ce7ede6bee162b11eacfa71a0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/lpr-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
