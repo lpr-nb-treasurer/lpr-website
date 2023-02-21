2049c150b15a0425c94ca249ee4c94a517df17a0
nb.treasurer <nb.treasurer@lambdapsirho.org>
2023-02-20 19:26:55 -0800

    Updates

---
+++ precache-manifest.254193202e25ec48237ef817d497f295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6dcaeca5f903acda7c",
+    "url": "/static/css/main.9f891846.chunk.css"
+  },
+  {
+    "revision": "1a6dcaeca5f903acda7c",
+    "url": "/static/js/main.9e7e2b3e.chunk.js"
+  },
+  {
+    "revision": "756a53bd0fb2c374f565",
+    "url": "/static/js/runtime~main.a5dcd33a.js"
+  },
+  {
+    "revision": "c14d811209f160f966f7",
+    "url": "/static/js/2.5a9919cf.chunk.js"
+  },
+  {
+    "revision": "e897de3d059808493f9f",
+    "url": "/static/js/3.5aaf3fca.chunk.js"
+  },
+  {
+    "revision": "17531ee9cfdd6f99523eb5d0b13252ce",
+    "url": "/index.html"
+  }
+];
