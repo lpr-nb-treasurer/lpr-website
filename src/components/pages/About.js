07c795dee619d9b09474f8f14905c203acce35a6
nb.treasurer <nb.treasurer@lambdapsirho.org>
2023-06-11 13:49:24 -0700

    made changes to readme and fixed ig profile position. also removed the about us video

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
   const title = 'About Us';
   const content =
-    'Lambda Psi Rho is an Asian-interest, but not Asian-exclusive, organization at the University of Nevada, Reno. We continuously promote brotherhood and diversity, foster leadership, succeed academically, and serve the community.';
+    'Lambda Psi Rho is an Asian-interest, but not Asian-exclusive, organization at the University of Nevada, Reno. We continuously promote brotherhood and diversity, foster leadership, succeed academically, and serve the community. We were established on February 11th, 2006, by 11 distinguished gentlemen, and the active house continues their legacy by upholding our pillars of Leadership, Pride, and Respect.';
   return (
     <main>
       <div className="main-content-wrapper">
@@ -11,15 +11,7 @@ export default () => {
           <h1 className="display-3">
             <strong>{title}</strong>
           </h1>
-          <div className="main-content-paragraph-wrapper">{content}</div>
-          <div id="about-page-video">
-            <iframe
-              src="https://www.youtube.com/embed/Ox1xyZEDMPc"
-              height="422"
-              title="Lambda Psi Rho Yardshow 2018"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
+          <div className="main-content-paragraph-wrapper">{content}
           </div>
         </div>
       </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,14 +9,14 @@ export default () => {
           <strong>Lambda Psi Rho</strong>
         </h1>
         <p>
-          <strong>Founding Chapter at The University of Nevada</strong>
+          Founding Chapter at The University of Nevada Est. 2006
         </p>
         <div className="button-wrapper">
           <Button id="about-us-button" href={'about'}>
             About Us
           </Button>
         </div>
-          <div id="ig-profile" align="center">
+        <div id="ig-profile">
           <blockquote className="instagram-media" data-instgrm-permalink="https://www.instagram.com/lambdapsirho/?utm_source=ig_embed&utm_campaign=loading" data-instgrm-version={14} style={{background: '#FFF', border: 0, borderRadius: '3px', boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)', margin: '0px', maxWidth: '540px', minWidth: '326px', padding: 0, width: '100%'}}>
             <div style={{padding: '16px'}}>
               <a href="https://www.instagram.com/lambdapsirho/?utm_source=ig_embed&utm_campaign=loading" style={{background: '#FFFFFF', lineHeight: 0, padding: '0 0', textAlign: 'center', textDecoration: 'none', width: '100%'}} target="_blank">
